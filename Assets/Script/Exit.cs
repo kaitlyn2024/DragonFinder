2008cd74b4739d0e8133a19bd2bb70ff742c1ae8
Kaitlym <kmmart47@asu.edu>
2024-03-17 22:28:17 -0700

    Update Exit.cs

--- Assets/Script/Exit.cs
+++ Assets/Script/Exit.cs
@@ -24,6 +24,7 @@ public class Exit : MonoBehaviour
             SceneManager.LoadScene("Menu");
         }
     }
+    //
     private void OnTriggerEnter(Collider other)
     {
         print(other.tag);
